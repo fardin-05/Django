8ee86fa6550b4b76f872a10f3de3e1265deaf916
Fardin Khan <azimahmed9722@gmail.com>
2025-12-01 21:31:35 +0600

    Remove unused import and comment in views.py

--- product/views.py
+++ product/views.py
@@ -1,4 +1,3 @@
-#from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
@@ -6,4 +5,3 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset=Product.objects.all().order_by('-created_at')
     serializer_class=ProductSerializer
 
-# Create your views here.
